20a24dfc58e783daddbc364ef0cda438671d1985
dcarianela <aki.hayakawa5577@gmail.com>
2025-09-04 11:59:14 -0300

    importando icon

--- App.tsx
+++ App.tsx
@@ -1,18 +1,38 @@
+import { useFonts } from "expo-font";
 import { StatusBar } from "expo-status-bar";
-import { Button, Image, StyleSheet, Text, View } from "react-native";
+import { Button, Image, Pressable, StyleSheet, Text, View } from "react-native";
 import { SafeAreaProvider, SafeAreaView }  from "react-native-safe-area-context";
 
+// Importando a biblioteca interna vector-icons
+import {Ionicons} from "@expo/vector-icons";
+
 export default function App() {
+  /* Usamos o useFonts para criar uma referência para a fonte que queremos utilizar no app. */
+  const [fonteCarregada] = useFonts ({
+    Monoton: require("./assets/fonts/Monoton-Regular.ttf")
+  });
+
+  // Se a fonte ainda não foi carregada
+  if (!fonteCarregada){
+    return (
+      <Text style={{ color: "red", fontSize: 22}}>Carregando fonte...</Text>
+    );
+  }
+
   return (
     <SafeAreaProvider>
       <SafeAreaView style={estilos.container}>
         <StatusBar style="auto"></StatusBar>
         <View style={estilos.viewLogo}>
           <Image source={require("./assets/dahora.png")} style={estilos.logo} />
-          <Text>Dá Hora Filmes</Text>
+          <Text style={estilos.tituloApp}>Dá Hora Filmes</Text>
         </View>
         <View style={estilos.viewBotoes}>
-          <Button title="Buscar Filmes" />
+          <Pressable style={estilos.botaoInicial}>
+            <Ionicons name="search" size={24} color="white" />
+            <Text style={estilos.textoBotao}>Buscar Filmes</Text>
+          </Pressable>
+
           <Button title="Favoritos" />
         </View>
         <View style={estilos.viewRodape}>
@@ -42,12 +62,27 @@ const estilos = StyleSheet.create({
     width: 128,
     height: 128,
   },
+  tituloApp: {
+    fontSize: 32,
+    color: "#5451a6",
+    fontFamily: "Monoton",
+  },
   viewBotoes: {
     backgroundColor: "#ffcc80",
     flex: 1,
     flexDirection: "row",
     justifyContent: "space-evenly",
     alignItems: "center",
+  },
+  botaoInicial: {
+    backgroundColor: "#5451a6",
+    padding: 16,
+    borderRadius: 8,
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  textoBotao: {
+    color: "white",
   },
   viewRodape: {
     backgroundColor: "#ef9a9a",
