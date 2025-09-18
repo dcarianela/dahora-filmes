768bbcb67391d99629176e824188c53025c7b105
dcarianela <aki.hayakawa5577@gmail.com>
2025-09-18 09:56:51 -0300

    Realiza ajustes

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,32 @@
-import { View, Text } from "react-native";
+import { useFonts } from "expo-font";
+import Loading from "../src/components/Loading";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { Stack } from "expo-router";
+import { StatusBar } from "react-native";
 
 export default function Layout() {
+  const [fonteCarregada] = useFonts({
+    Monoton: require("../assets/fonts/Monoton-Regular.ttf"),
+  });
+
+  if (!fonteCarregada) {
+    return <Loading />;
+  }
+
   return (
-    <View>
-      <Text>_layout</Text>
-    </View>
+    <SafeAreaProvider>
+      <StatusBar barStyle="default" backgroundColor="#5451a6" />
+      <Stack
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: "#5451a6",
+          },
+          headerTintColor: "#fff",
+          headerTitleStyle: {
+            fontWeight: "bold",
+          },
+        }}
+      />
+    </SafeAreaProvider>
   );
 }
