4aa9a9a534ba8499685142726c4558b130bbe3dc
dcarianela <aki.hayakawa5577@gmail.com>
2025-09-18 11:59:25 -0300

    bbb

--- app/index.tsx
+++ app/index.tsx
@@ -4,36 +4,53 @@ import { SafeAreaView } from "react-native-safe-area-context";
 import { View, Text, Image, Pressable, StyleSheet } from "react-native";
 // @ts-ignore
 import { Ionicons } from "@expo/vector-icons";
+import { Link, Stack } from "expo-router";
 
 export default function index() {
   return (
-    <SafeAreaView style={estilos.container}>
-      <StatusBar style="auto" />
-      <View style={estilos.viewLogo}>
-        <Image source={require("../assets/dahora.png")} style={estilos.logo} />
-        <Text style={estilos.tituloApp}>Dá Hora Filmes</Text>
-      </View>
-      <View style={estilos.viewBotoes}>
-        <Pressable style={estilos.botaoInicial}>
-          <Ionicons name="search" size={18} color="white" />
-          <Text style={estilos.textoBotao}> Buscar Filmes</Text>
-        </Pressable>
-        <Pressable style={estilos.botaoInicial}>
-          <Ionicons name="star" size={18} color="gold" />
-          <Text style={estilos.textoBotao}> Favoritos</Text>
-        </Pressable>
-      </View>
-      <View style={estilos.viewRodape}>
-        <Pressable style={estilos.botaoRodape}>
-          <Ionicons name="lock-closed" size={18} color="white" />
-          <Text style={estilos.textoBotao}> Privacidade</Text>
-        </Pressable>
-        <Pressable style={estilos.botaoRodape}>
-          <Ionicons name="information-circle" size={18} color="white" />
-          <Text style={estilos.textoBotao}> Sobre</Text>
-        </Pressable>
-      </View>
-    </SafeAreaView>
+    <>
+      <Stack.Screen options={{ headerShown: false, headerTitle: "Home" }} />
+      <SafeAreaView style={estilos.container}>
+        <StatusBar style="auto" />
+        <View style={estilos.viewLogo}>
+          <Image
+            source={require("../assets/dahora.png")}
+            style={estilos.logo}
+          />
+          <Text style={estilos.tituloApp}>Dá Hora Filmes</Text>
+        </View>
+        <View style={estilos.viewBotoes}>
+          <Link href="/buscar" asChild>
+            <Pressable style={estilos.botaoInicial}>
+              <Ionicons name="search" size={18} color="white" />
+              <Text style={estilos.textoBotao}> Buscar Filmes</Text>
+            </Pressable>
+          </Link>
+
+          <Link href="/favoritos" asChild>
+            <Pressable style={estilos.botaoInicial}>
+              <Ionicons name="star" size={18} color="gold" />
+              <Text style={estilos.textoBotao}> Favoritos</Text>
+            </Pressable>
+          </Link>
+        </View>
+        <View style={estilos.viewRodape}>
+          <Link href="/privacidade" asChild>
+            <Pressable style={estilos.botaoRodape}>
+              <Ionicons name="lock-closed" size={18} color="white" />
+              <Text style={estilos.textoBotao}> Privacidade</Text>
+            </Pressable>
+          </Link>
+
+          <Link href="/sobre" asChild>
+            <Pressable style={estilos.botaoRodape}>
+              <Ionicons name="information-circle" size={18} color="white" />
+              <Text style={estilos.textoBotao}> Sobre</Text>
+            </Pressable>
+          </Link>
+        </View>
+      </SafeAreaView>
+    </>
   );
 }
 
