00b8eaafd85ab0802c4e77babccbbab5e17ddf24
dcarianela <aki.hayakawa5577@gmail.com>
2025-09-25 11:59:29 -0300

    cria detalhes

--- src/components/CardFilme.tsx
+++ src/components/CardFilme.tsx
@@ -4,12 +4,20 @@ import { StyleSheet, Text, View, Pressable, Image } from "react-native";
 // @ts-ignore
 import { Ionicons } from "@expo/vector-icons";
 import { CardFilmeProps } from "../types";
+import { useRouter } from "expo-router";
 
 export default function CardFilme({ filme }: CardFilmeProps) {
   // Extraindo cada prop de dentro do filme
   const { id, title, poster_path } = filme;
-  console.log(id, title, poster_path);
 
+  // Importando o router para permitir a navegação através de programação
+  const router = useRouter();
+
+  const leiaMais = () => {
+    router.push({
+      pathname: `/detalhes/${id}`,
+    });
+  };
   return (
     <View style={estilos.card}>
       <Image
@@ -23,9 +31,9 @@ export default function CardFilme({ filme }: CardFilmeProps) {
         style={estilos.imagem}
       />
       <View style={estilos.corpo}>
-        <Text style={estilos.titulo}> Titulo do filme... </Text>
+        <Text style={estilos.titulo}> {title} </Text>
         <View style={estilos.botoes}>
-          <Pressable style={estilos.botao}>
+          <Pressable style={estilos.botao} onPress={leiaMais}>
             <Text style={estilos.textoBotao}>
               <Ionicons name="book" size={12} /> Leia mais
             </Text>
